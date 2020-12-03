a136d53744b68f71cf15b42ed928dd8e1563b6e9
Eric Sanchez <sanceric@oregonstate.edu>
2020-12-02 17:59:08 -0800

    Removed commented blocks from login.js

--- public/login.js
+++ public/login.js
@@ -1,9 +1,5 @@
 // Authors: Maddie Smith
 
-// Require fs to be able to read and write with userData.json
-//var path = require('path');
-//var fs = require('fs');
-
 // Get the submit button to add a new user
 var submitLogin = document.querySelector('.submit');
 
@@ -20,25 +16,16 @@ submitLogin.addEventListener('click', function(event) {
     var password = passwordInput.value;
 
     checkUser(username, password);
-
-    // Reset the input values to be used for the next time
-//    resetInputs(n_usernameInput, n_passwordInput, nv_passwordInput);
 });
 
 function checkUser(username, password) {
-    
-    // Add access for a user if the username and password meet the criteria
-//    if (verifyUser(username) && minimumRequirement(password)) {
-//    if (minimumRequirement(password)){
         var loggedUser = {
             Username: username,
             Password: password,
             Access: 1
         };
         var requestBody = JSON.stringify(loggedUser);
-
         sendResponseAndRequest(event, requestBody);
-//    }
 }
 
 function resetInputs(user, pass, vpass) {
@@ -48,13 +35,6 @@ function resetInputs(user, pass, vpass) {
 }
 
 function sendResponseAndRequest(event, requestBody) {
-
-    /*if (event.target.status !== 200) {
-        var responseToUser = event.target.response;
-        //alert("Error storing in database! " + responseToUser);
-    } else {
-        console.log("Successfully stored in database!");
-    }*/
    var requestURL = "/checkLogin"
     var req = new XMLHttpRequest();
     req.open("POST", requestURL, true);
@@ -76,41 +56,6 @@ function sendResponseAndRequest(event, requestBody) {
 );
    req.send(requestBody);
 }
-
-// Make sure that the username is unique and not taken
-//function verifyUser(username) {
-//    console.log("verifying username");
-//    minimumRequirement(username);
-//    let userData = fs.readFileSync('userData.json');
-//    let jUserData = JSON.parse(userData);
-//
-//    var i = 0;
-//
-//    for (i = 0; i < jUserData.length; i++) {
-//        console.log("Stored: ",jUserData[i].Username);
-//        if (jUserData[i].Username == username) {
-//            console.log("USERNAME EXISTS!", username);
-//            if (jUserData[i].Password == password) {
-//                console.log("LOGIN SUCCESSFUL!");
-//                return 1;
-//            }
-//        }
-//    }
-//    return 0;
-//};
-//
-//// Make sure that the username or password is at least 8 characters long
-//function minimumRequirement(entry) {
-//    console.log(entry);
-//    if (entry.length >= 8) {
-//        console.log("Is at least 8 char\n");
-//        return 1;
-//    }
-//    else {
-//        console.log("Must be at least 8 char\n");
-//        return 0;
-//    }
-//}
 
 /**
  * Model for User object, used to create new User
